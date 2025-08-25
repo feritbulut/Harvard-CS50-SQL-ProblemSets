cefc0b4c0c90f2a123c35525587ba32196d7613e
Ferit Bulut <115030629+feritbulut@users.noreply.github.com>
2025-08-25 22:30:17 +0300

    Add files via upload

---
+++ players/10.sql
@@ -0,0 +1,1 @@
+SELECT "first_name" AS 'First Name', "last_name" AS 'Last Name', "debut" AS 'First Game' FROM "players" WHERE "bats" = 'L' AND "birth_country" = 'USA' ORDER BY "debut" DESC;
---
+++ players/2.sql
@@ -0,0 +1,1 @@
+Select "bats" FROM "players" WHERE "first_name" = 'Babe' AND "last_name" = 'Ruth';
